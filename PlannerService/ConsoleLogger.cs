340e29b8480f860c2844270dbcf38b2bda995b3b
faester <faester@gmail.com>
2012-08-22 01:21:41 +0200

    Added missing file

---
+++ PlannerService/ConsoleLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PlannerService.DataLayer;
+
+namespace PlannerService
+{
+    class ConsoleLogger : ILogger
+    {
+        public void Debug(string p)
+        {
+            System.Diagnostics.Trace.WriteLine(p);
+        }
+
+        public void Info(string p)
+        {
+            System.Diagnostics.Trace.TraceInformation(p);
+        }
+    }
+}
